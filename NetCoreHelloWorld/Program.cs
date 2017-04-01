d9094be559dbcb751958f5744c905d9a7a71d03e
Leonardo Altieri <altieri.leon@gmail.com>
2017-04-01 16:43:04 +0200

    Aggiungere i file di progetto.

---
+++ NetCoreHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NetCoreHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine("\nHello, {0}, on {1:d} at {1:t}", name, date);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
