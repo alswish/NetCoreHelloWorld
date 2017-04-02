070485c31e5e677d7e71d5cbc29e7910542fdf3f
Leonardo Altieri <altieri.leon@gmail.com>
2017-04-02 21:36:35 +0200

    Web

---
+++ MyFirstWebProject/Data/MyFirstWebProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstWebProject.Models
+{
+    public class MyFirstWebProjectContext : DbContext
+    {
+        public MyFirstWebProjectContext (DbContextOptions<MyFirstWebProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstWebProject.Models.Movie> Movie { get; set; }
+    }
+}
